d4883bf2cf9a692e561c3b5ba12e30cc48356835
ssander2@uw.edu <ssander2@uw.edu>
2012-11-27 15:32:20 -0800

    Formatted a better conference creation dialog, except that only the title does anything currently. All of the date fields do nothing, but they are formatted.

--- src/edu/uwt/tcss360/Default/gui/ConferencesListPanel.java
+++ src/edu/uwt/tcss360/Default/gui/ConferencesListPanel.java
@@ -6,10 +6,14 @@
 package edu.uwt.tcss360.Default.gui;
 
 import java.awt.BorderLayout;
+import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
+import java.text.NumberFormat;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.Set;
 
@@ -17,12 +21,16 @@ import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.JButton;
+import javax.swing.JComponent;
+import javax.swing.JFormattedTextField;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.text.MaskFormatter;
 
 import edu.uwt.tcss360.Default.model.Conference;
 import edu.uwt.tcss360.Default.model.CurrentState;
@@ -39,7 +47,12 @@ import edu.uwt.tcss360.Default.model.User.Role;
 @SuppressWarnings("serial")
 public class ConferencesListPanel extends AbstractConferencesPanel
 {
-	
+	/**
+	 * Holds the dialog strings for the new conference creation dialog.
+	 */
+	public static final String[] DATE_TITLES = {"Start Date:", "End Date:", 
+		"Submission Deadline:", "Review Deadline:", 
+		"Recommendation Deadline:", "Final Submission Deadline:"};
 	
 	/**
 	 * the new conference button.
@@ -128,34 +141,92 @@ public class ConferencesListPanel extends AbstractConferencesPanel
 				getCurrentState().setCurrentRole(role);
 				getCurrentState().setCurrentConference(my_conference.getID());
 				//commented out until ConferencePanel is implemented.
-//				getPanelManager().pushPanel(new ConferencePanel(
-//						getCurrentState(), getPanelManager()));
+				getPanelManager().pushPanel(new ConferencePanel(
+						getCurrentState(), getPanelManager(), my_conference));
 			}
 		}
 		
 	}
 	
 	private class NewConferenceAction extends AbstractAction
-	{
+	{		
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
-			// TODO create a conference creation dialog class to get other info.
-			String conference_name = JOptionPane.showInputDialog("Enter Conference Name");
-			if (conference_name.trim().length() < 1)
+			JPanel creation_panel = new JPanel(new GridLayout(0,1));
+			JPanel name_panel = new JPanel(new FlowLayout());
+			name_panel.add(new JLabel("Conference Name:"));
+			JTextField name_field = new JTextField(30);
+			name_panel.add(name_field);
+			creation_panel.add(name_panel);
+			JPanel[] date_panels = new JPanel[DATE_TITLES.length];
+			JTextField[][] date_fields = new JTextField[date_panels.length][3];
+			for (int i = 0; i < date_panels.length; i++)
 			{
-				JOptionPane.showMessageDialog(new JFrame(), "Conference Name Invalid", "Error",
-				        JOptionPane.ERROR_MESSAGE);
+				date_panels[i] = new JPanel(new FlowLayout());
+				date_panels[i].add(new JLabel(DATE_TITLES[i]));
+				for (int j = 0; j < 3; j++)
+				{
+					String field_format = "**";//day or month
+					if (j == 2)//year
+						field_format = "****";
+					try
+					{
+						date_fields[i][j] = new JFormattedTextField(new MaskFormatter(field_format));
+					} catch (ParseException e1) {e1.printStackTrace();}
+					switch (j)
+					{
+					case 0: date_fields[i][j].setText("MM");
+						break;
+					case 1: date_fields[i][j].setText("DD");
+						break;
+					default: date_fields[i][j].setText("YYYY");
+					}
+					date_panels[i].add(date_fields[i][j]);
+					if (j != 2)
+						date_panels[i].add(new JLabel("/"));
+				}
+				
+				creation_panel.add(date_panels[i]);
 			}
-			else
+			int result = JOptionPane.showConfirmDialog(null, creation_panel, 
+		               "Create Conference", JOptionPane.OK_CANCEL_OPTION);
+			if (result == JOptionPane.OK_OPTION)
 			{
-				if (getCurrentState() == null)
-					System.out.println("asafsdsd");
-				User current = getCurrentState().getCurrentUser();
-				Conference new_conf = new Conference(current.getID(), conference_name, new Date());
-				getCurrentState().getConferencesManager().addConference(new_conf);
-				updatePanel();
+				//TODO: create conference given date information, not just the title.
+				String conference_name = name_field.getText();
+				if (conference_name.trim().length() < 1)
+				{
+					JOptionPane.showMessageDialog(new JFrame(), "Conference Name Invalid", "Error",
+					        JOptionPane.ERROR_MESSAGE);
+				}
+				else
+				{
+					User current = getCurrentState().getCurrentUser();
+					Conference new_conf = new Conference(current.getID(), conference_name, new Date());
+					getCurrentState().getConferencesManager().addConference(new_conf);
+					updatePanel();
+				}
 			}
+			
+			
+			
+			
+//			String conference_name = JOptionPane.showInputDialog("Enter Conference Name");
+//			if (conference_name.trim().length() < 1)
+//			{
+//				JOptionPane.showMessageDialog(new JFrame(), "Conference Name Invalid", "Error",
+//				        JOptionPane.ERROR_MESSAGE);
+//			}
+//			else
+//			{
+//				if (getCurrentState() == null)
+//					System.out.println("asafsdsd");
+//				User current = getCurrentState().getCurrentUser();
+//				Conference new_conf = new Conference(current.getID(), conference_name, new Date());
+//				getCurrentState().getConferencesManager().addConference(new_conf);
+//				updatePanel();
+//			}
 		}
 	}
 	
