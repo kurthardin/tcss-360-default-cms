beb8c1bee42858c80517ebac73452e4e6f33eebe
Kurt Hardin <kurthardin.dev@gmail.com>
2012-12-01 14:49:19 -0800

    Added check for null users_roles map. Fixed formatting.

--- src/edu/uwt/tcss360/Default/util/xml/ConferenceDocument.java
+++ src/edu/uwt/tcss360/Default/util/xml/ConferenceDocument.java
@@ -22,35 +22,41 @@ import edu.uwt.tcss360.Default.model.User.Role;
  */
 public class ConferenceDocument extends InfoDocument 
 {
-	public ConferenceDocument(File the_output_file) {
+	public ConferenceDocument(File the_output_file) 
+	{
 		super(the_output_file);
 	}
 	
-	public ConferenceDocument addMyUsersRolesField(Map<String, Set<Role>> the_value) {
-		// Create root "my_users_roles" XML element
-		Element users_roles_elem = my_document.createElement(
-				Conference.XML_ELEMENT_MY_USERS_ROLES);
-		
-		// Create a sub-element in "my_users_roles" for each user
-		for (String user_id : the_value.keySet()) {
-			Element user_elem = my_document.createElement(
-					User.XML_ELEMENT_USER);
-			user_elem.setAttribute(Conference.XML_ATTR_ID, user_id);
+	public ConferenceDocument addMyUsersRolesField(Map<String, Set<Role>> the_value) 
+	{	
+		if (the_value != null) 
+		{
+			// Create root "my_users_roles" XML element
+			Element users_roles_elem = my_document.createElement(
+					Conference.XML_ELEMENT_MY_USERS_ROLES);
 			
-			// Create a sub-element in "user" for each Role
-			for (Role role : the_value.get(user_id)) {
-				Element role_elem = my_document.createElement(
-						Conference.XML_ELEMENT_ROLE);
-				role_elem.setAttribute(Conference.XML_ATTR_NAME, role.name());
+			// Create a sub-element in "my_users_roles" for each user
+			for (String user_id : the_value.keySet()) 
+			{
+				Element user_elem = my_document.createElement(
+						User.XML_ELEMENT_USER);
+				user_elem.setAttribute(Conference.XML_ATTR_ID, user_id);
 				
-				user_elem.appendChild(role_elem);
+				// Create a sub-element in "user" for each Role
+				for (Role role : the_value.get(user_id)) 
+				{
+					Element role_elem = my_document.createElement(
+							Conference.XML_ELEMENT_ROLE);
+					role_elem.setAttribute(Conference.XML_ATTR_NAME, role.name());
+					
+					user_elem.appendChild(role_elem);
+				}
+				
+				users_roles_elem.appendChild(user_elem);
 			}
 			
-			users_roles_elem.appendChild(user_elem);
+			my_fields_element.appendChild(users_roles_elem);
 		}
-		
-		my_fields_element.appendChild(users_roles_elem);
-		
 		return this;
 	}
 	
